97a8e7f825e82e41f4378a8d5c18d82c4472a99a
Indicaza <zachreitmeier@yahoo.com>
2024-04-02 23:25:52 -0400

    Improved <Pantry> component styling

--- src/components/Pantry/Pantry.jsx
+++ src/components/Pantry/Pantry.jsx
@@ -11,20 +11,36 @@ const Pantry = () => {
   }, []);
 
   return (
-    <div className="pantry-grid">
-      {pantryItems.map((item, index) => (
-        <div key={index} className="pantry-item">
-          <div className="pantry-item-header">
-            <h3>{item.Name}</h3>
+    <section className="pantry">
+      <div className="pantry-grid">
+        {pantryItems.map((item, index) => (
+          <div key={index} className="pantry-item">
+            <div className="pantry-item-names">
+              <h3>{item.Name}</h3>
+            </div>
+            <div className="pantry-item-tags">
+              <h4>~{item.Tags.join(', ')}~</h4>
+            </div>
+            <div className="pantry-image-container">
+              <img
+                src={item.ImageURL}
+                alt={item.Name}
+                className="pantry-image"
+              />
+            </div>
+            {/* Wrap the quantity/unit and expiration date in a new div with class name */}
+            <div className="pantry-item-details">
+              <p className="pantry-item-quantity">
+                Qty: {`${item.Quantity} ${item.Unit}`}
+              </p>
+              <p className="pantry-item-expiration">
+                Exp: {item.ExpirationDate}
+              </p>
+            </div>
           </div>
-          <div className="pantry-image-container">
-            <img src={item.ImageURL} alt={item.Name} className="pantry-image" />
-          </div>
-          <p>{`${item.Quantity} ${item.Unit}`}</p>
-          <p>Exp: {item.ExpirationDate}</p>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
