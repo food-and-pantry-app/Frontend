8be3c2161a078a19735b1bf519fcc68ee5b79d53
Indicaza <zachreitmeier@yahoo.com>
2024-04-22 09:03:07 -0400

    update

--- src/components/Pantry/Pantry.jsx
+++ src/components/Pantry/Pantry.jsx
@@ -7,29 +7,43 @@ const Pantry = () => {
   const [pantryItems, setPantryItems] = useState([]);
 
   useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/api/pantry/');
-        setPantryItems(response.data);
-      } catch (error) {
-        console.error('Error fetching pantry items:', error);
-      }
-    };
     fetchItems();
   }, []);
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/pantry/');
+      setPantryItems(response.data);
+    } catch (error) {
+      console.error('Error fetching pantry items:', error);
+    }
+  };
+
+  const deleteItem = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/api/pantry/${id}`);
+      const newItems = pantryItems.filter((item) => item.ID !== id);
+      setPantryItems(newItems);
+    } catch (error) {
+      console.error('Error deleting pantry item:', error);
+    }
+  };
 
   return (
     <section className="pantry">
       <div className="pantry-grid">
         {pantryItems.map((item, index) => {
-          // Ensure Tags is an array
           const tagsArray = Array.isArray(item.Tags)
             ? item.Tags
             : item.Tags.split(', ');
           return (
             <div key={index} className="pantry-item">
-              <div className="pantry-item-names">
-                <h3>{item.Name}</h3>
+              <div className="pantry-item-header">
+                <h3 className="title">{item.Name}</h3>
+                <button
+                  className="button-delete"
+                  onClick={() => deleteItem(item.ID)}
+                />
               </div>
               <div className="pantry-item-tags" style={getTagStyle(tagsArray)}>
                 <h4>~{tagsArray.join(', ')}~</h4>
